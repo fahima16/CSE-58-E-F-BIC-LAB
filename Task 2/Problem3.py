06ce9e9cc46a188f11a9294b5392a738cfee93b7
fahima16 <144542931+fahima16@users.noreply.github.com>
2026-06-09 15:56:42 +0600

    Implement DNA base pairing in Problem3.py

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,15 @@
+str = input()
 
+rev = ""
+
+for i in str:
+    if i == 'A':
+        rev = 'T' + rev
+    elif i == 'T':
+        rev = 'A' + rev
+    elif i == 'C':
+        rev = 'G' + rev
+    elif i == 'G':
+        rev = 'C' + rev
+    
+print (rev)
