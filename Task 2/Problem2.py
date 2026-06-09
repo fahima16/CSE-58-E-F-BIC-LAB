e578747821cd91b1532a34da2819b1033e9a4c8c
fahima16 <144542931+fahima16@users.noreply.github.com>
2026-06-09 15:55:44 +0600

    Implement substring frequency counter
    Count occurrences of substrings of a given length and print the most frequent ones.

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,20 @@
+str = input()
 
+num= int(input())   
+
+count = {}
+
+length = len(str)
+
+for i in range(length - num + 1):
+    substr = str[i:i+num]
+    if substr in count:
+        count[substr] += 1
+    else:
+        count[substr] = 1
+
+max_count = max(count.values())
+
+for substr in count:
+    if count[substr] == max_count:
+        print (substr, end=" ")
