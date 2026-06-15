56c6f80c7e958e225ed36369cff4023f0c1001bc
fahima16 <144542931+fahima16@users.noreply.github.com>
2026-06-15 23:25:24 +0600

    Correct loop range for pattern matching in Problem1.py
    Fix off-by-one error in loop range for pattern matching.

--- Task 2/Problem1.py
+++ Task 2/Problem1.py
@@ -1,1 +1,12 @@
+text = input()
+pattern = input()
 
+length = len(pattern)
+
+count = 0;
+
+for i in range(len(text)-length+1):
+    if(text[i:i+length] == pattern):
+        count += 1
+
+print(count)
