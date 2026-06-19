621ea82ad0605e29c52279c086aeb082605287e8
fahima16 <144542931+fahima16@users.noreply.github.com>
2026-06-19 21:20:41 +0600

    Add pattern matching with allowed mismatches
    Implement pattern matching with mismatches for DNA sequences.

--- Task 3/Problem1.py
+++ Task 3/Problem1.py
@@ -1,1 +1,43 @@
+text = input()
 
+#k = int(input())
+#d = int(input())
+
+k, d = map(int, input().split())
+
+#k, d = input().split()
+#k = int(k)
+#d = int(d)
+
+letters = ['A', 'C', 'G', 'T']
+
+pattern = [""] 
+
+for i in range(k):
+    new_pat = []
+    for p in pattern:
+        for ch in letters:
+            new_pat.append(p + ch)
+    pattern = new_pat
+
+count = {}
+
+for pat in pattern:
+    total = 0
+
+    for i in range(len(text)-k+1):
+        mismatch = 0
+
+        for j in range(k):
+            if(pat[j] != text[i+j]):
+                mismatch += 1
+
+        if(mismatch <=d):
+            total += 1
+
+    count[pat] = total
+
+max_count = max(count.values())
+for pat in count:
+    if(count[pat] == max_count):
+        print(pat, end = " ")
